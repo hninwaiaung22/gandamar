14698f4088b95ae7f0de2f09f32252b8af163a9e
hninwaiaung <hninwaiaung926@gmail.com>
2019-09-16 22:41:27 +0630

    Update index.js

--- index.js
+++ index.js
@@ -142,6 +142,23 @@ requestify.post(sendmessageurl,
   "type": "postback",
   "title": "button 3",
   "payload": "payload 3"
+},
+{
+      "title":"SaPal Phyu",
+	  "image_url":"https://sites.psu.edu/siowfa16/files/2016/10/YeDYzSR-10apkm4.png",
+      "subtitle":"test",
+      "buttons":[{
+  "type": "postback",
+  "title": "button 1",
+  "payload": "payload 1"
+},{
+  "type": "postback",
+  "title": "button 2",
+  "payload": "payload 2"
+},{
+  "type": "postback",
+  "title": "button 3",
+  "payload": "payload 3"
 }
 	  ]      
     }]
